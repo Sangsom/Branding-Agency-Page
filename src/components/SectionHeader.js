0fd5b79b77070b298e5391a306de72fc1dd81d15
Rinalds Pudulis <rpudulis@gmail.com>
2018-07-03 13:11:27 +0300

    feat: Added Section Header styled component

---
+++ src/components/SectionHeader.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const SectionHeader = styled.h2`
+    font-family: "Playfair Display", serif;
+    font-size: 38px;
+    font-weight: 700;
+    letter-spacing: 0.025em;
+    color: #171717;
+`;
+
+export default SectionHeader;
