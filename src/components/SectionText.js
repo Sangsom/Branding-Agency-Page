21f818e3d628b966e5d5103f0f6108647ff80e2d
Rinalds Pudulis <rpudulis@gmail.com>
2018-07-03 13:11:42 +0300

    feat: Added Section Text styled component

---
+++ src/components/SectionText.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const SectionText = styled.p`
+    font-family: "Work Sans", sans-serif;
+    color: #5f5f5f;
+    max-width: 40%;
+    text-align: center;
+`;
+
+export default SectionText;
