c876dba97845c0cfe3e4c62ca429c4d9f1fadd72
Rinalds Pudulis <rpudulis@gmail.com>
2018-07-05 15:45:46 +0300

    fix: Removed unused variables

--- src/components/BestProjects.js
+++ src/components/BestProjects.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import SectionHeader from "./SectionHeader";
 import SectionText from "./SectionText";
